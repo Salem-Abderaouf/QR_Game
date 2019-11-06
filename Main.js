2ea60ad4b685a983a7da43f0a1f7f6375b2da799
Salem-Abderaouf <salamasalem84@gmail.com>
2019-11-06 12:41:37 +0100

    import vector-icons package

--- Main.js
+++ Main.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import { StyleSheet, Platform, Image, Text, View ,Button} from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome';
 import firebase from 'react-native-firebase'
 
 export default class Main extends React.Component {
+  static navigationOptions = {
+    title:'Profile',
+    headerRight:() => (
+      <Icon.Button
+       name="facebook"
+       onPress={() => alert('Hola From fb ')}
+       backgroundColor="#3b5998"
+      />
+    )
+  }
     state = { currentUser: null }  
   componentDidMount() {
-      const { currentUser } = firebase.auth()
+      const { currentUser } =  firebase.auth()
       this.setState({ currentUser })
   }
   render() {
