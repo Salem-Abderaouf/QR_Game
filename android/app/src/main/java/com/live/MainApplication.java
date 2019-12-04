f94b501922ea1e7655dc977028176149b10c564c
Salem-Abderaouf <salamasalem84@gmail.com>
2019-12-04 17:51:39 +0100

    import react-native-camera package

--- android/app/src/main/java/com/live/MainApplication.java
+++ android/app/src/main/java/com/live/MainApplication.java
@@ -2,6 +2,7 @@ package com.live;
 
 import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.auth.RNFirebaseAuthPackage;
+import org.reactnative.camera.RNCameraPackage;
 import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
@@ -26,7 +27,6 @@ public class MainApplication extends Application implements ReactApplication {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
           packages.add(new RNFirebaseAuthPackage());
           return packages;
         }
